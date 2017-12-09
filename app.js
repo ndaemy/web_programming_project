3d0bd48960400039bc2ad90d8e464ad0de61909d
yuyaebean <yuyaebean@naver.com>
2017-12-09 16:11:25 +0900

    Make signin / signout feature

--- app.js
+++ app.js
@@ -5,9 +5,15 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var sassMiddleware = require('node-sass-middleware');
+var session = require('express-session');
+var methodOverride = require('method-override');
+var flash = require('connect-flash');
+var mongoose   = require('mongoose');
+var passport = require('passport');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var passportConfig = require('./lib/passport-config');
 
 var app = express();
 
@@ -15,22 +21,65 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Pug의 local에 moment라이브러리와 querystring 라이브러리를 사용할 수 있도록.
+app.locals.moment = require('moment');
+app.locals.querystring = require('querystring');
+
+//=======================================================
+// mongodb connect
+//=======================================================
+mongoose.Promise = global.Promise; // ES6 Native Promise를 mongoose에서 사용한다.
+const connStr = 'mongodb://localhost/ebcpdb';
+// 아래는 mLab을 사용하는 경우의 예: 본인의 접속 String으로 바꾸세요.
+// const connStr = 'mongodb://dbuser1:mju12345@ds113825.mlab.com:13825/sampledb1';
+mongoose.connect(connStr, {useMongoClient: true });
+mongoose.connection.on('error', console.error);
+
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// _method를 통해서 method를 변경할 수 있도록 함. PUT이나 DELETE를 사용할 수 있도록.
+app.use(methodOverride('_method', {methods: ['POST', 'GET']}));
+
 app.use(sassMiddleware({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   indentedSyntax: false, // true = .sass and false = .scss
   sourceMap: true
 }));
+
+// session을 사용할 수 있도록.
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: 'long-long-long-secret-string-1313513tefgwdsvbjkvasd'
+}));
+
+app.use(flash()); // flash message를 사용할 수 있도록
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+//=======================================================
+// Passport 초기화
+//=======================================================
+app.use(passport.initialize());
+app.use(passport.session());
+passportConfig(passport);
+
+// pug의 local에 현재 사용자 정보와 flash 메시지를 전달하자.
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;  // passport는 req.user로 user정보 전달
+  res.locals.flashMessages = req.flash();
+  next();
+});
 
 app.use('/', index);
 app.use('/users', users);
+require('./routes/auth')(app, passport);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,74 @@
 var express = require('express');
-var router = express.Router();
+const User = require('../models/user');
+const router = express.Router();
+const catchErrors = require('../lib/async-error');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+function needAuth(req, res, next) {
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    req.flash('danger', 'Please signin first.');
+    req.redirect('/signin');
+  }
+}
+
+function validateForm(form, options) {
+  var name = form.name || "";
+  var email = form.email || "";
+  name = name.trim();
+  email = email.trim();
+
+  if (!name) {
+    return 'Name is required.';
+  }
+
+  if (!email) {
+    return 'Email is required.';
+  }
+
+  if (!form.password && options.needPassword) {
+    return 'Password is required.';
+  }
+
+  if (form.password !== form.password_confirmation) {
+    return 'Password do not match.';
+  }
+
+  if (form.password.length < 8) {
+    return 'Password must be at least 8 characters.';
+  }
+
+  return null;
+}
+
+router.get('/', needAuth, function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/new', (req, res, next) => {
+  res.render('users/new', {messages: req.flash()});
+});
+
+router.post('/', catchErrors(async (req, res, next) => {
+  var err = validateForm(req.body, {needPassword: true});
+  if (err) {
+    req.flash('danger', err);
+    return res.redirect('back');
+  }
+  var user = await User.findOne({email: req.body.email});
+  console.log('USER??', user);
+  if (user) {
+    req.flash('danger', 'Email address already exists.');
+    return res.redirect('back');
+  }
+  user = new User({
+    name: req.body.name,
+    email: req.body.email,
+  });
+  user.password = await user.generateHash(req.body.password);
+  await user.save();
+  req.flash('success', 'Resigered successfully. Please sign in.');
+  res.redirect('../');
+}));
 
 module.exports = router;
